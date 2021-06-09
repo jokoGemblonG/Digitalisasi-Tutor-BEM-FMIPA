ca46fe19676d7dd59e40d49d6ec3b489c010ddd3
jokoGemblonG <85027718+jokoGemblonG@users.noreply.github.com>
2021-06-09 17:53:31 +0700

    Update config.php

--- PHP-Digital/config.php
+++ PHP-Digital/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = pg_connect('host=localhost dbname=Digitalisasi user=postgres password="sesuain ya ges"');
+$db = pg_connect('host=localhost dbname=db_tutor user=postgres password="  "');
 
 if( !$db ){
     die("Gagal terhubung dengan database: " . pg_connect_error());}
