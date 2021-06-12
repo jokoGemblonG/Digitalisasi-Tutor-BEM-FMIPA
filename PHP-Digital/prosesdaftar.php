33d78d514a884917735a687b6fe65c6e0791a4b5
Tony Alfan <85027718+jokoGemblonG@users.noreply.github.com>
2021-06-12 22:07:48 +0700

    Update prosesdaftar.php

--- PHP-Digital/prosesdaftar.php
+++ PHP-Digital/prosesdaftar.php
@@ -1,17 +1,19 @@
 <?php
 	include('config.php');
 	if(isset($_POST['daftar'])){
-		$nama = $_POST['Nama_pendaftar']
+		$id_pendaftar = $_POST['id_pendaftar'];
+		$IDK = $_POST['IDK'];
+		$nama = $_POST['Nama_pendaftar'];
 		$kontak = $_POST['kontak'];
 		$email = $_POST['email'];
-		$id_availability = $_POST['id_availability'];
-		$paket = $_POST['paket']
+		$paket = $_POST['paket'];
 		
-		$query = pg_query("INSERT INTO tabel (Nama_pendaftar, kontak, email, id_availability, paket) 
-		VALUES('$Nama_pendaftar', '$kontak', '$email', '$id_availability', 'paket')");
+		$query = pg_query("INSERT INTO pendaftar (id_pendaftar, idk, nama_pendaftar, no_hp, email, paket) VALUES('$id_pendaftar','$IDK', '$nama', '$kontak', '$email', '$paket')");
+		
 		if($query == TRUE){
 			header('Location: index.php?status=sukses');
 			}
 		else{
 			header('Location: index.php?status=gagal');}
 	}?>
+	
