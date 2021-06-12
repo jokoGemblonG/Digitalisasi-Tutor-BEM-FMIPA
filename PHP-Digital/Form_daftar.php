635e392a32daa8e6217223870f17942dd1c1b404
Tony Alfan <85027718+jokoGemblonG@users.noreply.github.com>
2021-06-12 22:13:59 +0700

    Update Form_daftar.php

--- PHP-Digital/Form_daftar.php
+++ PHP-Digital/Form_daftar.php
@@ -16,40 +16,50 @@
 		<div class="container-fluid">
 		<ul class="nav navbar-nav">
 			<li class="active"><a href="index.php">Home</a></li>
-			<li><a href="login.php"> Login Tutor</a></li>
 			<li><a href="list tutor.php"> List Tutor</a></li>
 			<li><a href="Form_daftar.php"> Pendaftaran</a></li>
+		</ul>
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="login.php"> Login Tutor</a></li>
 		</ul>
 	  </div>
 	 </nav>
 	<body>
 	
 		<h3 style="text-align:center;">Formulir Pendaftaran Peserta Tutor</h3>
-		<formaction="prosesdaftar.php" method="POST">
-		<fieldset>
-		<p>
-			<label for='Nama_pendaftar'>Nama : </label></br>
-			<input type='text' name='nama' placeholder='Nama Lengkap'/>
-		</p>
-		<p>
-			<label for='kontak'>Kontak : </label></br>
-			<input type='text' name='kontak' placeholder='No HP'>	
-		</p>	
-		<p>
-			<label for='email'>Email : </label></br>
-			<input type='email' name='email' placeholder='Email'>
-		</p>
-		<p>
-			<label for='id_availability'>Kode Tutor : </label></br>
-			<input type='text' name='id_availability' placeholder='Kode Tutor'>
-		<p>
-			<label for='paket'>Paket : </label>
-				</br>
-				<label><input type='radio' name='paket' value='A'>Paket A</label>
-				</br>
-				<label><input type='radio' name='paket' value='B'>Paket B</label>
-				</br>
-				<label><input type='radio' name='paket' value='C'>Paket C</label>
-		</p>
-	</fieldset></form>
+		<form action="prosesdaftar.php" method="POST">
+			<fieldset>
+			<p>
+				<label for='id_pendaftar'>NIM : </label></br>
+				<input type='text' name='id_pendaftar' placeholder='A14190000'>
+			</p>
+			<p>
+				<label for='IDK'>Kode Kelas : </label></br>
+				<input type='text' name='IDK' placeholder='Kode Kelas: BIOSEN160001'>
+			</p>
+			<p>
+				<label for='Nama_pendaftar'>Nama : </label></br>
+				<input type='text' name='Nama_pendaftar' placeholder='Nama Lengkap'/>
+			</p>
+			<p>
+				<label for='kontak'>Kontak : </label></br>
+				<input type='text' name='kontak' placeholder='No HP'>	
+			</p>	
+			<p>
+				<label for='email'>Email : </label></br>
+				<input type='email' name='email' placeholder='Email'>
+			</p>
+			<p>
+				<label for='paket'>Paket : </label>
+					</br>
+					<label><input type='radio' name='paket' value='A'>Paket A</label>
+					</br>
+					<label><input type='radio' name='paket' value='B'>Paket B</label>
+					</br>
+					<label><input type='radio' name='paket' value='C'>Paket C</label>
+			</p>
+			<p>
+				<input type='submit' value='daftar' name='daftar'/>
+			</p>
+		</fieldset></form>
 	</body></html>
