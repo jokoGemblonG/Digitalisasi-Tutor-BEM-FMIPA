f86a7d7eaa6e1e7d55aa00a348fe35ba65445028
Tony Alfan <85027718+jokoGemblonG@users.noreply.github.com>
2021-06-16 21:36:09 +0700

    Update Fis.php

--- PHP-Digital/Fis.php
+++ PHP-Digital/Fis.php
@@ -1,8 +1,17 @@
+<?php 
+	include("config.php");
+	session_start();
+	if(isset($_SESSION['submit'])){
+		header("Location: index2.php");
+		exit;
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-	<title> Tutor Bem FMIPA</title>
+	<title> Tutor BEM FMIPA | Fisika </title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -12,20 +21,50 @@
 
 <body>
 	<header>
-		<h3> Situs Terpadu </h3>
-		<h2> Tutor Bem FMIPA </h2>
+		<h4 style='padding: 3px'> SITUS TERPADU </h4>
+		<h2 style='padding: 3px'> Tutor BEM FMIPA </h2>	
 	</header>
 	
-	<nav>
+	<nav class= "navbar navbar-inverse">
 		<div class="container-fluid">
 		<ul class="nav navbar-nav">
 			<li class="active"><a href="index.php">Home</a></li>
 			<li><a href="Form_daftar.php"> Pendaftaran</a></li>
 		</ul>
 		<ul class="nav navbar-nav navbar-right">
-			<li><a href="login.php"> Login Tutor</a></li>
+			<li><a href="login.php"> Login (Tutor)</a></li>
 		</ul>
 	  </div>
-	 </nav>
-	 
+	</nav>
+	
+	<table border="2">
+	<thead>
+		<tr>
+			<th style='text-align:center'>ID Kelas</th>
+			<th style='text-align:center'>Hari</th>
+			<th style='text-align:center'>Jam</th>
+			<th style='text-align:center'>Tutor</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php
+		$query = pg_query("SELECT K.idk AS idk, hari, jam, nama FROM (((kelas K INNER JOIN sesi S ON K.ids = S.ids) INNER JOIN tutor T ON K.id_tutor = T.id_tutor) INNER JOIN mata_kuliah M ON K.idm = M.idm) WHERE K.idm = 'FIS'");
+
+		while($kelas = pg_fetch_array($query)){
+			echo "<tr>";
+
+			echo "<td  style='padding: 3px'>".$kelas['idk']."</td>";
+			echo "<td style='padding: 3px'>".$kelas['hari']."</td>";
+			echo "<td style='padding: 3px'>".$kelas['jam']."</td>";
+			echo "<td style='padding: 3px'>".$kelas['nama']."</td>";
+
+			echo "</tr>";
+			}
+		?>
+
+	</tbody>
+
+	</table>
+	
 	</body></html>
