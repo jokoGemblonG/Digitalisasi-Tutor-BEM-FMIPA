4838e79a0428d4bc6321a0a1cb9feea6e41dc9eb
Tony Alfan <85027718+jokoGemblonG@users.noreply.github.com>
2021-06-16 21:42:20 +0700

    Update index2.php

--- PHP-Digital/index2.php
+++ PHP-Digital/index2.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+$idtutor = $_SESSION['idtutor'];
+if(!isset($_SESSION['submit'])){
+	header("Location: login.php");
+	exit;
+	}
+include('config.php');
+?>
 <!DOCTYPE html>
 <html>
 
@@ -12,20 +21,104 @@
 
 <body>
 	<header>
-		<h3> Situs Terpadu </h3>
-		<h2> Tutor Bem FMIPA </h2>
+		<h4 style='padding: 3px'> SITUS TERPADU </h4>
+		<h2 style='padding: 3px'> Tutor BEM FMIPA </h2>	
 	</header>
 	
-	<nav>
-		<div class="container-fluid">
+	<nav class= "navbar navbar-inverse">
+	<div class="container-fluid">
 		<ul class="nav navbar-nav">
 			<li class="active"><a href="index2.php">My Home</a></li>
-			<li><a href="datamurid.php"> Data murid</a></li>
 		</ul>
 		<ul class="nav navbar-nav navbar-right">
 			<li><a href="logout.php"> Logout</a></li>
 		</ul>
 	  </div>
 	 </nav>
-	 
+	
+	<div class="container table-responsive p-0">
+		<table class="table" border="2">
+		<thead>
+			<tr>
+				<th style='text-align:center'>ID Kelas</th>
+				<th style='text-align:center'>Hari</th>
+				<th style='text-align:center'>Jam</th>
+				<th style='text-align:center'>Jumlah Peserta</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			$query = pg_query("SELECT K.idk AS idk, hari, jam, COUNT(distinct DH.id_pendaftar) AS jumlah_murid FROM ((((kelas K INNER JOIN sesi S ON K.ids = S.ids) INNER JOIN tutor T ON K.id_tutor = T.id_tutor) INNER JOIN mata_kuliah M ON K.idm = M.idm) INNER JOIN daftar_hadir DH ON K.idk = DH.idk) WHERE K.id_tutor = '$idtutor' GROUP BY K.idk, hari, jam ORDER BY K.idk, hari, jam");
+			while($kelas = pg_fetch_array($query)){
+				echo "<tr>";
+
+				echo "<td  style='padding: 3px'>".$kelas['idk']."</td>";
+				echo "<td style='padding: 3px'>".$kelas['hari']."</td>";
+				echo "<td style='padding: 3px'>".$kelas['jam']."</td>";
+				echo "<td style='padding: 3px'>".$kelas['jumlah_murid']."</td>";
+
+				echo "</tr>";
+				}
+			?>
+		</tbody>
+		</table></div><br />
+	<br />
+
+	<!-- Tabel Daftar Hadir -->
+	<div class="container table-responsive p-0">
+		<table class="table" border="2">
+		<thead>
+			<tr>
+				<th style='text-align:center'>ID Kelas</th>
+				<th style='text-align:center'>Nama Murid</th>
+				<!-- <th style='text-align:center'>Jam</th>
+				<th style='text-align:center'>Tutor</th> -->
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			$query = pg_query("SELECT DH.idk AS idk, P.nama_pendaftar AS murid FROM ((daftar_hadir DH INNER JOIN kelas K ON DH.idk = K.idk) INNER JOIN pendaftar P ON DH.id_pendaftar = P.id_pendaftar) WHERE K.id_tutor = '$idtutor';");
+			while($kelas = pg_fetch_array($query)){
+				echo "<tr>";
+
+				echo "<td  style='padding: 3px'>".$kelas['idk']."</td>";
+				echo "<td style='padding: 3px'>".$kelas['murid']."</td>";
+				/* echo "<td style='padding: 3px'>".$kelas['jam']."</td>";
+				echo "<td style='padding: 3px'>".$kelas['nama']."</td>"; */
+
+				echo "</tr>";
+				}
+			?>
+		</tbody>
+		</table></div><br />
+	<br />
+	
+	<!-- Tabel Upah Tutor -->
+	<div class="container table-responsive p-0">
+		<table class="table" border="2">
+		<thead>
+			<tr>
+				<th style='text-align:center'>Upahmu</th>
+				<!-- <th style='text-align:center'>Nama Murid</th>
+				<th style='text-align:center'>Jam</th>
+				<th style='text-align:center'>Tutor</th> -->
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			$query = pg_query("SELECT CONCAT('Rp', SUM(upah_tutor)) upah_tutor FROM (SELECT CASE WHEN paket = 'A' THEN 12000*COUNT(DH.id_pendaftar) WHEN paket = 'B' THEN 8000*COUNT(DH.id_pendaftar) WHEN paket = 'C' THEN 4000*COUNT(DH.id_pendaftar) END AS upah_tutor FROM ((daftar_hadir DH INNER JOIN kelas K ON DH.idk = K.idk) INNER JOIN pendaftar P ON DH.id_pendaftar = P.id_pendaftar) WHERE K.id_tutor = '$idtutor' GROUP BY P.paket) AS x;");
+			while($kelas = pg_fetch_array($query)){
+				echo "<tr>";
+
+				echo "<td style='padding: 3px'>".$kelas['upah_tutor']."</td>";
+				/* echo "<td style='padding: 3px'>".$kelas['murid']."</td>";
+				echo "<td style='padding: 3px'>".$kelas['jam']."</td>";
+				echo "<td style='padding: 3px'>".$kelas['nama']."</td>"; */
+
+				echo "</tr>";
+				}
+			?>
+		</tbody>
+		</table></div><br />
+	<br />	
 	</body></html>
